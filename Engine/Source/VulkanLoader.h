82d23f3466ad4f99455256ddec8ff77c091d9a41
Euxiniar <charles.ago37@gmail.com>
2018-04-04 10:04:32 +0200

    create swapchain

--- Engine/Source/VulkanLoader.h
+++ Engine/Source/VulkanLoader.h
@@ -10,15 +10,28 @@
 #include <memory>
 #include <vector>
 #include <set>
+#include <algorithm>
 
 namespace Ly {
 	struct QueueFamilyIndices {
+		//Queue Family wich supports graphics
 		int graphicsFamily = -1;
+		//Queue Family wich supports display presentation
 		int presentFamily = -1;
 
 		bool matches() {
 			return graphicsFamily >= 0 && presentFamily >= 0;
 		}
+	};
+
+	struct SwapChainSupportDetails {
+		//Min/Max number of images in the swapchain
+		//Min/Max images size in the swapchain
+		VkSurfaceCapabilitiesKHR capabilities;
+		//Pixel Format/Color Space
+		std::vector<VkSurfaceFormatKHR> formats;
+		//Image resolution in the swapchain
+		std::vector<VkPresentModeKHR> presentModes;
 	};
 
 	class VulkanLoader
@@ -36,8 +49,15 @@ namespace Ly {
 		void createSurface();
 		void pickPhysicalDevice();
 		bool isDeviceSuitable(VkPhysicalDevice device);
+		bool checkDeviceExtensionSupport(VkPhysicalDevice device);
 		QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
 		void createLogicalDevice();
+		SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
+		VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
+		VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
+		VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
+		void createSwapChain();
+
 
 		const std::vector<const char*> m_validationLayers = {
 			"VK_LAYER_LUNARG_standard_validation"
@@ -47,6 +67,11 @@ namespace Ly {
 #else
 		const bool enableValidationLayers = true;
 #endif
+
+		const std::vector<const char*> m_deviceExtensions = {
+			VK_KHR_SWAPCHAIN_EXTENSION_NAME
+		};
+
 		std::unique_ptr<Ly::Window>& m_window;
 		VkInstance m_instance;
 		std::string m_appName = "Vulkan";
@@ -57,5 +82,9 @@ namespace Ly {
 		VkDevice m_device;
 		VkQueue m_graphicsQueue;
 		VkQueue m_presentQueue;
+		VkSwapchainKHR m_swapChain;
+		std::vector<VkImage> m_swapChainImages;
+		VkFormat m_swapChainImagesFormat;
+		VkExtent2D m_swapChainExtent;
 	};
 }
